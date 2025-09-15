724a60cebc9de898f8dbac954eb1fa38fad22c7d
Michael Busbee <busbee.michael101@gmail.com>
2025-09-15 16:39:54 -0400

    Continuation off working on protected routes and sending and detecting JWT.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,30 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
+import { LocalAuthGuard } from './local-auth.guard';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  // @HttpCode(HttpStatus.OK)
-  // @Post('login')
-  // signIn(@Body() signInDto: SignInDto) {
-  //   return this.authService.signIn(signInDto);
-  // }
+  @HttpCode(HttpStatus.OK)
+  @UseGuards(AuthGuard('local'))
+  @Post('/login')
+  async login(@Request() req) {
+    return req.user;
+  }
+  @UseGuards(LocalAuthGuard)
+  @Post('/logout')
+  async logout(@Request() req) {
+    return req.logout();
+  }
 }
